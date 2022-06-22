bff5ee2356d7fd2da010d887ac140c3878f9172c
unknown <1291357321@qq.com>
2022-06-22 23:11:43 +0800

    mypush

---
+++ my-site/my-site/src/mock/setting.js
@@ -0,0 +1,20 @@
+import Mock from "mockjs";
+Mock.mock("/api/setting", "get", {
+  code: 0,
+  msg: "",
+  data: {
+    avatar: "http://mdrs.yuanjin.tech/Fs4CDlC6mwe_WXLMIiXcmSJLHO4f",
+    siteTitle: "我的个人空间",
+    github: "https://github.com/liboran12",
+    qq: "1241732165",
+    qqQrCode:
+      "https://pan.baidu.com/s/1aQMeCIetChsuQjvVEdGhIQ",
+    weixin: "L241732165F", 
+    weixinQrCode:
+      "https://pan.baidu.com/s/1aQMeCIetChsuQjvVEdGhIQ",
+    mail: "1241732165@qq.com",
+    icp: "黑ICP备17001719号",
+    githubName: "liboran12",
+    favicon: "http://mdrs.yuanjin.tech/Fs4CDlC6mwe_WXLMIiXcmSJLHO4f",
+  },
+});
